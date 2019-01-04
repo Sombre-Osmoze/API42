c0cee20e8b8fc3f51adc689cb23870d4fb44876b
Marcus Florentin <fmarcus28@gmx.fr>
2019-01-04 13:09:03 +0100

    Initial Commit

---
+++ API42Tests/API42Tests.swift
@@ -0,0 +1,34 @@
+//
+//  API42Tests.swift
+//  API42Tests
+//
+//  Created by Marcus Florentin on 04/01/2019.
+//  Copyright © 2019 Marcus Florentin. All rights reserved.
+//
+
+import XCTest
+@testable import API42
+
+class API42Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
