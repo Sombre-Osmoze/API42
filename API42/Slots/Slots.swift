6c6774925ba6a179c51aaa442db1017b6b1985cb
Marcus Florentin <fmarcus28@gmx.fr>
2019-05-23 00:21:46 +0200

    Bad naming of the structure

--- API42/Slots/Slots.swift
+++ API42/Slots/Slots.swift
@@ -11,7 +11,7 @@ import Foundation
 /// A Slot is a time interval when a user desclares himself available to evaluate other users.
 /// Actually, a slot must be at least 30 minutes (with a granularity of 15 minutes).
 /// A slot can be set every day between 30 minutes and 2 weeks in advance.
-public struct Slots {
+public struct Slot {
 
 	public let id : ID
 
