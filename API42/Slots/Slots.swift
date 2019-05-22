565b4ca92a1804a0020159c34f96b683b8341c73
Marcus Florentin <fmarcus28@gmx.fr>
2019-05-23 00:11:45 +0200

    Create Slots implementation:     • Slots structure     • TODO: Slots Tests

---
+++ API42/Slots/Slots.swift
@@ -0,0 +1,22 @@
+//
+//  Slots.swift
+//  API42
+//
+//  Created by Marcus Florentin on 22/05/2019.
+//  Copyright © 2019 Marcus Florentin. All rights reserved.
+//
+
+import Foundation
+
+/// A Slot is a time interval when a user desclares himself available to evaluate other users.
+/// Actually, a slot must be at least 30 minutes (with a granularity of 15 minutes).
+/// A slot can be set every day between 30 minutes and 2 weeks in advance.
+public struct Slots {
+
+	public let id : ID
+
+	public let beginAt : Date
+
+	public let endAt : Date
+
+}
