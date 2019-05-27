5b3225f2931844f441f97221de8e9d99f1a761e5
Marcus Florentin <fmarcus28@gmx.fr>
2019-05-27 17:32:09 +0200

    Starting Projects tests:     • Codable tests     • Decodable tests

--- API42Tests/ProjectsTests.swift
+++ API42Tests/ProjectsTests.swift
@@ -1,15 +1,15 @@
 //
-//  Slots.swift
+//  ProjectsTests.swift
 //  API42Tests
 //
-//  Created by Marcus Florentin on 22/05/2019.
+//  Created by Marcus Florentin on 27/05/2019.
 //  Copyright © 2019 Marcus Florentin. All rights reserved.
 //
 
 import XCTest
 @testable import API42
 
-class SlotsTests: XCTestCase {
+class ProjectsTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,8 +18,6 @@ class SlotsTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-	// MARK: - Codable
 
 	private lazy var decoder : JSONDecoder = {
 		let dec = JSONDecoder()
@@ -36,41 +34,32 @@ class SlotsTests: XCTestCase {
 	/// Test if the slot structure is conforme to the Codable protocol.
 	func testCodable() {
 
-		/// A Slot structure
-		let slot = Slot(id: 1, beginAt: Date(timeIntervalSinceReferenceDate: 5),
-						endAt: Date(timeIntervalSinceReferenceDate: 125),
-						scaleTeam: nil,
-						user: "invisible")
- 
+		/// A Project structure
+		let project = Project(id: 1, name: "Test", slug: "Test", parentid: nil)
+
 		var data : Data! = nil
 		var result : Slot! = nil
 
-		XCTAssertNoThrow(data = try JSONEncoder().encode(slot), "Not conforme to encodable protocol")
+		XCTAssertNoThrow(data = try JSONEncoder().encode(project), "Not conforme to encodable protocol")
 
-		XCTAssertNoThrow(result = try JSONDecoder().decode(Slot.self, from: data), "Not conforme to encodable protocol")
+		XCTAssertNoThrow(result = try JSONDecoder().decode(Project.self, from: data), "Not conforme to encodable protocol")
 
 		guard result != nil else { return }
 
-		XCTAssertEqual(slot, result, "After encoding and decoding the data are not the same")
+		XCTAssertEqual(project, result, "After encoding and decoding the data are not the same")
 
 	}
 
-	/// Test if the slot structure is conforme to Decodable proctocole with some API datas.
 	func testDecodable() {
+		let file = URL(fileURLWithPath: "Project.json")
+		guard let data = try? Data(contentsOf: file) else {
+			XCTFail("Can't load test file")
+			return
+		}
 
-		/// A slot's datas in UTF-8 format
-		let slotData = """
-{
-	"id": 27,
-	"begin_at": "2017-11-24T20:15:00.000Z",
-	"end_at": "2017-11-24T20:30:00.000Z",
-	"scale_team": null,
-	"user": "invisible"
-}
-""".data(using: .utf8)!
-
-		XCTAssertNoThrow(try decoder.decode(Slot.self, from: slotData), "Doesn't conforme to decodable protocol")
-
+		XCTAssertNoThrow(try decoder.decode(Project.self, from: data), "Doesn't conforme to decodable protocol")
 	}
+
+	
 
 }
