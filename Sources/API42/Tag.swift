75b84c8bef0ba4b0212088b3b9489fe51889e11e
Marcus Florentin <fmarcus28@gmx.fr>
2019-06-26 14:21:20 +0200

    Adding 'Tag' structure

---
+++ Sources/API42/Tag.swift
@@ -0,0 +1,18 @@
+//
+//  Tag.swift
+//  API42
+//
+//  Created by Marcus Florentin on 12/06/2019.
+//
+
+import Foundation
+
+public struct Tag: CodingAPI {
+
+	public let id : ID
+
+	public let name : String
+
+	public let kind : String
+
+}
